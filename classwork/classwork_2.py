acaf79977de02a0f2c2ea3c2f4b5e82247488ac7
Trineeth <VattipulusuTrineeth@gmail.com>
2025-08-04 13:44:45 -0400

    identify us

---
+++ classwork/classwork_2.py
@@ -0,0 +1,14 @@
+# print("Welcome to the world of python")
+# print(4)
+# print("hello python \n")
+# print("hello",5)
+# print("welcome to", end= "*") 
+# x = 5
+# y = "John"
+# print (x)
+# print (y)
+# name = input("enter your name")
+# print ("\n Hello" , name , "\n welcome to codinal" )
+import keyword
+print ("python keyword are...\n")
+print(keyword.kwlist)
