52b78a65efc1c7a12c5fb14548bb2a7c1be7f53b
Trineeth <VattipulusuTrineeth@gmail.com>
2025-08-05 13:41:20 -0400

    goo goo gaa gaa

---
+++ classwork/classwork_3.py
@@ -0,0 +1,24 @@
+# str1 = 'Hello'
+# str2 = ' world' 
+# print('str1 +  str2 =', str1 +  str2)
+# x = 5
+# y = 2
+# print ('x * y =', x * y)
+# str = 'Hello world'
+# print(str[::3])
+# print(str[0::1])
+# print(str[::-1])
+a = 5
+print("type of a: ", type(a))
+b = 2.5
+print("type of b: ", type(b))
+c = 'coding'
+print("type of c: ", type(c))
+d = True
+print("type of d: ", type(d))
+a = str(a)
+print("type of a: ", type(a))
+b = int(b)
+print("type of b: ", type(b))
+print (b)
+print(a - b)
